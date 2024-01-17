ee19af5496b44609a9d82b8edec84fec7bc86060
namk0016 <gornganok.nk@gmail.com>
2024-01-17 15:53:15 +0700

    news

--- src/page/News.js
+++ src/page/News.js
@@ -1,67 +1,75 @@
 import React from 'react';
 import './style.css';
 import Image from "../img/pic1.png";
+import{Link} from 'react-router-dom';
 
 const News = props => {
     return (
         <div className='item'> 
-            <div className='newsleft'>
-                <img src={Image} className='picsize'/>
-                <div className='contenthead'>
-                    <br/>หัวข้อ
-                </div>
-                <div className='textcontent'>
-                    เนื้อหา---------------------------------------------------------<br/>
-                    -------------------------------------------------------------
-                </div>
-            </div>
-            <div>
-                <div className='directionnews'>
-                    <div className='boxcover'>
-                        &emsp;บทความยอดนิยม
-                    </div>
-                    <div className='scroll'>
-                        <div >
-                            <img src={Image} className='piccontent'/>
-                            <div>
-                                <br/>เนื้อหา------------------------------------------------<br/>
-                                -------------------------------------------------------<br/>
-                                -------------------------------------------------------
-                            </div>
+            <div className='boxcover'>
+                <img src={Image} className='piccontent'/>
+                    <div>
+                        <br/><Link to="https://www.manarom.com/blog/toxic_person.html">Toxic Person รับมืออย่างไร เมื่อคนใกล้ตัวกลายเป็นพิษ</Link><br/>
                         
-                        </div>
-                        <div className='boxcontent'></div>
-                        <div>
-                            <img src={Image} className='piccontent'/>
-                            <div>
-                                <br/>เนื้อหา------------------------------------------------<br/>
-                                -------------------------------------------------------<br/>
-                                -------------------------------------------------------
-                            </div>
-                        </div>
-                        <div className='boxcontent'></div>
-                        <div>
-                            <img src={Image} className='piccontent'/>
-                            <div>
-                                <br/>เนื้อหา------------------------------------------------<br/>
-                                -------------------------------------------------------<br/>
-                                -------------------------------------------------------
-                            </div>
-                        </div>
-                        <div className='boxcontent'></div>
-                        <div>
-                            <img src={Image} className='piccontent'/>
-                            <div>
-                                <br/>เนื้อหา------------------------------------------------<br/>
-                                -------------------------------------------------------<br/>
-                                -------------------------------------------------------
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                    </div>       
             </div>
-
-        </div>   
+            <div className='boxcover'>
+                <img src={Image} className='piccontent'/>
+                    <div>
+                        <br/>เนื้อหา------------------------------------------------<br/>
+                        -------------------------------------------------------<br/>
+                        -------------------------------------------------------
+                    </div>
+            </div>
+            <div className='boxcover'>
+                <img src={Image} className='piccontent'/>
+                    <div>
+                        <br/>เนื้อหา------------------------------------------------<br/>
+                        -------------------------------------------------------<br/>
+                        ------------------------------------------------------
+                    </div>
+            </div>
+            <div className='boxcover'>
+                <img src={Image} className='piccontent'/>
+                    <div>
+                        <br/>เนื้อหา------------------------------------------------<br/>
+                        -------------------------------------------------------<br/>
+                        -------------------------------------------------------
+                    </div>
+            </div>
+            <div className='boxcontent'>
+                <img src={Image} className='piccontent'/>
+                    <div>
+                        <br/>เนื้อหา------------------------------------------------<br/>
+                        -------------------------------------------------------<br/>
+                        -------------------------------------------------------
+                    </div>
+            </div>
+            <div className='boxcontent'>
+                <img src={Image} className='piccontent'/>
+                    <div>
+                        <br/>เนื้อหา------------------------------------------------<br/>
+                        -------------------------------------------------------<br/>
+                        -------------------------------------------------------
+                    </div>
+            </div>
+            <div className='boxcontent'>
+                <img src={Image} className='piccontent'/>
+                    <div>
+                        <br/>เนื้อหา------------------------------------------------<br/>
+                        -------------------------------------------------------<br/>
+                        -------------------------------------------------------
+                    </div>
+            </div>
+            <div className='boxcontent'>
+                <img src={Image} className='piccontent'/>
+                    <div>
+                        <br/>เนื้อหา------------------------------------------------<br/>
+                        -------------------------------------------------------<br/>
+                        -------------------------------------------------------
+                    </div>
+            </div>
+        </div>
     );
 };
 export default News
