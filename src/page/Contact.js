236bb94a833f60f49d9cfb68f1d6f047ff7919c4
Yaowapa64 <yaowapapkumm@gmail.com>
2024-02-07 16:49:32 +0700

    contact002

--- src/page/Contact.js
+++ src/page/Contact.js
@@ -15,20 +15,17 @@ const Contact = props => {
             </div> 
 
             <div className='Contact-Report'>
-                <div className='contact0'>
-                    <br/>Contact
                     <div className='contactrow' style={{justifyContent:"center"}}>
-                        <div>
-                            <img src={backgroundcon} className='backgroundcon'/> 
-                            <div className='contact1'>
+                        <div style={{width:"40%", backgroundColor:"#e77575", marginLeft:"40px", padding:"10px", borderRadius:"30px", boxShadow:"0 4px 10px"}}>
+                            <div className='contact1'style={{color:"white"}}>
                                 คลินิกเวชกรรม สจล. คณะแพทยศาสตร์
                             </div>
                             <div>
                                 <div className='contactrow'>
-                                    <div style={{width:"75px"}}>
+                                    <div style={{width:"75px", color:"white"}}>
                                         &emsp;เปิดทำการ
                                     </div>
-                                    <div>
+                                    <div style={{color:"white"}}>
                                        &emsp;&emsp;&emsp;จันทร์ - ศุกร์ เวลา 09:00 น. - 17:00 น.<br/>
                                        &emsp;&emsp;&emsp;เสาร์ - อาทิตย์ เวลา 09.00 น. - 11.00 น.<br/>
                                        &emsp;&emsp;&emsp;วันหยุดนักขัตฤกษ์ เวลา 09:00 น. - 17:00 น.
@@ -39,10 +36,10 @@ const Contact = props => {
                                 <div>
                                 <img src={location} className='piclocation'/> 
                                 </div>
-                                    <div style={{width:"75px"}}>
+                                    <div style={{width:"75px",color:"white"}}>
                                         &emsp;ที่ตั้ง 
                                     </div>
-                                    <div>
+                                    <div style={{color:"white"}}>
                                     &emsp;สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง <br/>
                                     &emsp;เลขที่ 1 ฉลองกรุง 1 เขตลาดกระบัง กรุงเทพมหานคร 10520
                                     </div>
@@ -51,10 +48,10 @@ const Contact = props => {
                                 <div>
                                 <img src={call} classname='picphone' style={{ height:"25px",width:"25px"}}/>
                                 </div>
-                                    <div style={{width:"75px"}}>
+                                    <div style={{width:"75px",color:"white"}}>
                                     &emsp;โทรศัพท์
                                     </div>
-                                    <div>
+                                    <div style={{color:"white"}}>
                                         &emsp;02 329 8143 <br/>
                                         &emsp;02 329 8000 ต่อ 3633<br/><br/>
                                     </div>
@@ -63,18 +60,15 @@ const Contact = props => {
                         </div>
                         
                     
-                        <div>
-                         <img src={backdeveloper} className='backdeve'/>
-                            <div className='contact1'>
+                        <div style={{width:"40%", backgroundColor:"#37ad92", marginLeft:"40px", padding:"10px", borderRadius:"30px", boxShadow:"0 4px 10px"}}>
+                            <div className='contact1' style={{color:"white"}}>
                                 Developer
                             </div>
                         </div>
-                    </div>
-
-                </div>
-                <div>
+                    </div><br/><br/>
+                <div style={{width:"35%", backgroundColor:"#ffdb58", margin:"auto", padding:"20px", borderRadius:"30px", boxShadow:"0 4px 10px"}}>
                     <div className='contact0'>
-                        <br/>Report problem<br/>
+                        Report problem<br/>
                         <form>
                             <label>
                                 <input 
